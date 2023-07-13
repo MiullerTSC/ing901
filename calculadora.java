7ee6036de7f9fbebec77ac81eaa076cd36d4cf06
MiullerTSC <139181750+MiullerTSC@users.noreply.github.com>
2023-07-12 20:33:35 -0600

    Delete calculadora.java

--- calculadora.java
+++
@@ -1,8 +0,0 @@
-public class Calculadora {
-
-    // Función que suma dos números enteros
-    public int sumar(int numero1, int numero2) {
-        int resultado = numero1 + numero2;
-        return resultado;
-    }
-}
