af230109cb81a8d6145953064b0c76c9835c6f8a
MiullerTSC <139181750+MiullerTSC@users.noreply.github.com>
2023-07-12 20:32:53 -0600

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora {
+
+    // Función que suma dos números enteros
+    public int sumar(int numero1, int numero2) {
+        int resultado = numero1 + numero2;
+        return resultado;
+    }
+}
